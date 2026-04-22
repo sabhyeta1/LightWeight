f7bcab0bd139c5fc0f9fd3fc7ed082e6e6dd79cf
Filip Lukic <filiplukic9@gmail.com>
2026-04-22 12:31:29 +0200

    fix: use auth middleware for workoutPlans

--- backend/src/routes/workoutPlanRoutes.js
+++ backend/src/routes/workoutPlanRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const workoutPlanController = require('../controllers/workoutPlanController');
+const { requireAuth } = require('../middlewares/authMiddleware')
 
 // beim erstellen aufgerufen
-router.post('/', workoutPlanController.createWorkoutPlan);
+router.post('/', requireAuth, workoutPlanController.createWorkoutPlan);
 // gibt alle pläne eines Users zurück
-router.get('/', workoutPlanController.getWorkoutPlans);
+router.get('/', requireAuth, workoutPlanController.getWorkoutPlans);
 
 module.exports = router;
